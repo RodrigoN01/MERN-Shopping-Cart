c7646ce41a992dc01e0965c84f86b1c92ad62227
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-01-20 17:21:34 +1300

    product screen responsive design

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -19,7 +19,28 @@ const ProductScreen = () => {
           </p>
         </div>
       </div>
-      <div className='productscreen__right'></div>
+      <div className='productscreen__right'>
+        <div className='right__info'>
+          <p>
+            Price: <span>$499.99</span>
+          </p>
+          <p>
+            Status: <span>In Stock</span>
+          </p>
+          <p>
+            Qty
+            <select>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+            </select>
+          </p>
+          <p>
+            <button type='button'>Add To Cart</button>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
